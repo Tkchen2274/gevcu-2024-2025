62fcab49df9fda85808b2c415dc9bd69373710ba
atsai488 <austintsai2014@gmail.com>
2024-04-11 19:30:59 -0700

    test

--- src/Statemachine.h
+++ src/Statemachine.h
@@ -24,10 +24,23 @@ enum State {
 extern State extern_curr_state; // Declaration of a global variable
 
 =======
-#define S0 "S0"
-#define S1 "S1"
-#define S2 "S2"
+// #define S0 "S0"
+// #define S1 "S1"
+// #define S2 "S2"
+
+// #define S0 0
+// #define S1 1
+// #define S2 2
+
+enum State {
+    S0,
+    S1,
+    S2
+};
+
 #define CFG_TICK_INTERVAL_HEARTBEAT                 2000000
+
+extern State extern_curr_state; // Declaration of a global variable               2000000
 >>>>>>> 4d8799480cdf1d39d9fc84fa8616d65cfae3ccc3
 // struct RawSignalData {
 //     int32_t input1; // e.g. pot #1 or the signal from a can bus throttle
@@ -42,7 +55,7 @@ public:
 <<<<<<< HEAD
   std::string getState();
 =======
-  // std::string getState();
+  std::string getState();
 >>>>>>> 4d8799480cdf1d39d9fc84fa8616d65cfae3ccc3
   void setup();
 
@@ -52,7 +65,7 @@ private:
   // std::string curr_state;
   State curr_state;
 =======
-  std::string curr_state;
+  // std::string curr_state;
 >>>>>>> 4d8799480cdf1d39d9fc84fa8616d65cfae3ccc3
   // analog value from the pedal
   // digital signal form button
