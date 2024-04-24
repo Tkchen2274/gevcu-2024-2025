d572a1a3af839c236d077968fa91510b7cd75ce6
Tkchen2274 <tkchen@usc.edu>
2024-04-23 19:04:16 -0700

    statemachine

--- src/Statemachine.cpp
+++ src/Statemachine.cpp
@@ -18,42 +18,70 @@ void Statemachine::updateState(State x){extern_curr_state = x;}
 
 void Statemachine::handleTick() {
 
+  // assume that brake 1 is depressed
+  // assume that tsms is okay if it's 1
+
+  brake = systemIO.getDigitalIn(0);
+  tsms = systemIO.getDigitalIn(1);
+  r2d = systemIO.getDigitalIn(2);
   switch (extern_curr_state)
   {
-    /*
-    State 0: Idle car
-    State 1: Signal to car to prepare for driving
-    State 2: Motor responds to APPS
-    if (State 0 and Ready to drive button){
-      send signal to dash
-      update to State 1
-    }
-    if (State 1 and NOT Tractive system master switch)
-    {
-      update to state 0
-    }
-    if (State 1 and Tracktive system master switch)
-    {
-      update to state 2
-    }
-    if (State 2 and Not tractive system Master Switch)
-    {
-      update to state 1
-    }
-    */
-    case S0:
-      updateState(S1);
-      Logger::console("\nI am in state S0 ");
-      // SerialUSB.print('0');
-    case S1:
+    if (extern_curr_state == S0) {
+      Logger::console("\nI am in state S0");
+      Logger::console(brake);
+      Logger::console(tsms);
+      Logger::console(r2d);
+      // extern_curr_state = S1;
+      if(brake && tsms && r2d){
+        updateState(S1);
+      } else {
+        updateState(S0);
+      }
+      
+      
+      SerialUSB.print('0');
+
+    } else if (extern_curr_state == S1) {
+
+      if(tsms && brake && r2d){
+        update(S2);
+      } else {
+        update(S0);
+      }
+      // extern_curr_state = S2;
       updateState(S2);
-      Logger::console("\nI am in state S1 ");
-      // SerialUSB.print('1');
-    case S2:
-      updateState(S0);
-      Logger::console("\nI am in state S2 ");
-      // SerialUSB.print('2');
+      Logger::console("\nI am in state S1");
+      SerialUSB.print('1');
+
+    } else if (extern_curr_state == S2) {
+      // gevcu is connected to low voltage
+      if(tsms){
+        update(S2);
+      } else {
+        update(S0);
+      }
+      // extern_curr_state = S0;
+      Logger::console("\nI am in state S2");
+      SerialUSB.print('2');
+    }
   }
+  
+
+
+    // case S0:
+    //   updateState(S1);
+    //   Logger::console("\nI am in state S0 ");
+      
+      
+    //   // SerialUSB.print('0');
+    // case S1:
+    //   updateState(S2);
+    //   Logger::console("\nI am in state S1 ");
+    //   // SerialUSB.print('1');
+    // case S2:
+    //   updateState(S0);
+    //   Logger::console("\nI am in state S2 ");
+      // SerialUSB.print('2');
   
   // if (extern_curr_state == S0) {
   //   // extern_curr_state = S1;
@@ -74,4 +102,26 @@ void Statemachine::handleTick() {
   //   SerialUSB.print('2');
 
   // }
+
+  /*
+    State 0: Idle car
+    State 1: Signal to car to prepare for driving
+    State 2: Motor responds to APPS
+    if (State 0 and Ready to drive button){
+      send signal to dash
+      update to State 1
+    }
+    if (State 1 and NOT Tractive system master switch)
+    {
+      update to state 0
+    }
+    if (State 1 and Tracktive system master switch)
+    {
+      update to state 2
+    }
+    if (State 2 and Not tractive system Master Switch)
+    {
+      update to state 1
+    }
+    */
 }
